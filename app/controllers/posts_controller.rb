12e52cd7433a5f2e25c206f85bf0af67522ca88b
Angelo <valmocinajayangelo@gmail.com>
2019-07-13 17:24:45 +0800

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,14 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update,:destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def new
@@ -10,13 +18,33 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to posts_path, notice: "You created a post!"
+      redirect_to @post, notice: "You created a post!"
     else 
       render :new, alert: "Your post wasn't created."
     end   
   end
+ 
+  def update 
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated!"
+    else
+      render :edit, alert: "Your post wasn't updated."
+    end
+  end
 
-  private def post_params
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Your post have been deleted!"
+  end
+
+  private 
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+  def post_params
     params.require(:post).permit(:title,:body)
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get '/',to: 'posts#index'
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  post "posts", to: "posts#create"
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  patch 'posts/:id', to: 'posts#update'
+
+  delete "posts/:id", to: 'posts#destroy', as: :delete_post
+  get 'posts/:id', to: 'posts#show', as: :post
+  post 'posts', to: 'posts#create'
 
 end
 
